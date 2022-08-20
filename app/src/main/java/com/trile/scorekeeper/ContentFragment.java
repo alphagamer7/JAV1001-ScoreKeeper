9a20299204bde7d9d995f4228441d5182a5bebdf
Athif Shaffy <athif.shaffy@gmail.com>
2022-08-20 09:33:51 +0530

    CLeaned up unused code

--- app/src/main/java/com/trile/scorekeeper/ContentFragment.java
+++ app/src/main/java/com/trile/scorekeeper/ContentFragment.java
@@ -2,8 +2,6 @@ package com.trile.scorekeeper;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatDelegate;
-
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -13,11 +11,7 @@ import android.widget.Button;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ContentFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ContentFragment extends Fragment {
 
     TextView team1ScoreTxt;
@@ -37,45 +31,13 @@ public class ContentFragment extends Fragment {
     int team2ScoreChangeBy = 1; // change by value for team 2 score
 
 
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public ContentFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ContentFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ContentFragment newInstance(String param1, String param2) {
-        ContentFragment fragment = new ContentFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
